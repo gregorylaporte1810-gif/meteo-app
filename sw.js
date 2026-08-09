de69f5cc9248629b1694f0de1ff464e0d1b56ba3
Gregory Boehm Belin <boehmbelin.gregory@gmail.com>
2026-08-09 23:23:36 +0200

    fix: changement de sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,14 @@
-self.addEventListener("install", (e) => self.skipWaiting());
-self.addEventListener("fetch", (e) => e.respondWith(fetch(e.request).catch(() => new Response("Hors ligne"))));
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  // Force la récupération sur le réseau sans passer par le cache HTTP
+  e.respondWith(
+    fetch(e.request, { cache: "no-store" }).catch(() => new Response("Hors ligne"))
+  );
+});
